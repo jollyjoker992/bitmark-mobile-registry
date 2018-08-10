f947843c7c78ea082ae614bc67280e4d1d5f2f8b
Dung Le <phieudu241@gmail.com>
2018-08-10 11:05:49 +0700

    Correct Http method (DELETE -> POST) for "Try Register Account"

--- client-app/src/models/notification-model.js
+++ client-app/src/models/notification-model.js
@@ -32,7 +32,7 @@ const doDeregisterNotificationInfo = (accountNumber, timestamp, signature, token
     let statusCode;
     let tempURL = `${config.mobile_server_url}/api/push_uuids/${token}`;
     fetch(tempURL, {
-      method: 'POST',
+      method: 'DELETE',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
@@ -57,7 +57,7 @@ const doTryRegisterAccount = (accountNumber, timestamp, signature) => {
     let statusCode;
     let tempURL = `${config.mobile_server_url}/api/events/register-account`;
     fetch(tempURL, {
-      method: 'DELETE',
+      method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
