4302ccd889d2243c8b23e9a3394def5045db7590
Aaron Chang <aaron.chang@quadas.com>
2018-09-04 14:06:23 +0800

    e2e-appium: disable warning dialog when in e2e test

--- client-app/App.js
+++ client-app/App.js
@@ -3,13 +3,20 @@ import { Text } from 'react-native';
 
 Text.defaultProps.allowFontScaling = false;
 
-import {
-  BitmarkAppComponent,
-  CodePushMainAppComponent
-} from './src';
+import { BitmarkAppComponent, CodePushMainAppComponent } from './src';
 
 let ApplicationComponent = BitmarkAppComponent;
-if (DeviceInfo.getBundleId() === 'com.bitmark.registry' || DeviceInfo.getBundleId() === 'com.bitmark.registry.inhouse') {
+if (
+  DeviceInfo.getBundleId() === 'com.bitmark.registry' ||
+  DeviceInfo.getBundleId() === 'com.bitmark.registry.inhouse'
+) {
   ApplicationComponent = CodePushMainAppComponent;
+} else {
+  // disable warning/exception messages, suggest by dung to avoid the exception of
+  // 'Could not invoke HeadlessJsTaskSupport.notifyTaskFinished'
+  // steps to generate exception:
+  // first time open app => restore account => 24 words => enable face id => exception
+  console.disableYellowBox = true;
+  console.reportErrorsAsExceptions = false;
 }
 export default ApplicationComponent;
