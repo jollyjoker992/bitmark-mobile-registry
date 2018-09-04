204e99dfb3332c49a6477d643cfaa705b13f8c54
Aaron Chang <aaron.chang@quadas.com>
2018-09-04 13:56:34 +0800

    e2e-appium: add new target

---
+++ client-app/ios/Pods/Target Support Files/Pods-Bitmark Registry dev e2e/Pods-Bitmark Registry dev e2e-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bitmark_Registry_dev_e2e : NSObject
+@end
+@implementation PodsDummy_Pods_Bitmark_Registry_dev_e2e
+@end
---
+++ client-app/ios/Pods/Target Support Files/Pods-Bitmark Registry dev e2e/Pods-Bitmark Registry dev e2e-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Bitmark_Registry_dev_e2eVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Bitmark_Registry_dev_e2eVersionString[];
+
