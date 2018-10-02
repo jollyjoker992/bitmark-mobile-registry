c740f59a100c41da2bf4be07a5e66570bd6aa4cd
Dung Le <phieudu241@gmail.com>
2018-10-02 14:34:31 +0700

    Localization - Implement for Application Detail screens

--- client-app/src/components/home/account/application-detail/application-detail.component.js
+++ client-app/src/components/home/account/application-detail/application-detail.component.js
@@ -34,28 +34,28 @@ export class ApplicationDetailComponent extends React.Component {
       Share.share({ title: 'Bitmark', message: '', url: config.appLink });
     };
     const requestSendFeedback = () => {
-      Alert.alert('Send Feedback', 'Have a comment or suggestion? We are always making improvements based on community feedback', [{
-        text: 'Cancel', style: 'cancel',
+      Alert.alert(global.i18n.t("ApplicationDetailComponent_sendFeedbackTitle"), global.i18n.t("ApplicationDetailComponent_sendFeedbackMessage"), [{
+        text: global.i18n.t("ApplicationDetailComponent_cancel"), style: 'cancel',
       }, {
-        text: 'Send', onPress: sendFeedback,
+        text: global.i18n.t("ApplicationDetailComponent_send"), onPress: sendFeedback,
       }]);
     };
 
     const rateApp = () => {
-      Alert.alert('App Store Review', 'Positive App Store ratings and reviews help support Bitmark. How would you rate us?', [{
-        text: '5 Stars!',
+      Alert.alert(global.i18n.t("ApplicationDetailComponent_appStoreReviewTitle"), global.i18n.t("ApplicationDetailComponent_appStoreReviewMessage"), [{
+        text: global.i18n.t("ApplicationDetailComponent_5Stars"),
         style: 'cancel',
         onPress: () => { Linking.openURL(config.appLink) }
       }, {
-        text: '4 Stars or less', onPress: requestSendFeedback,
+        text: global.i18n.t("ApplicationDetailComponent_4StarsOrLess"), onPress: requestSendFeedback,
       }]);
     }
 
     const sendFeedback = () => {
       Mailer.mail({
-        subject: 'Suggestion for Bitmark iOS',
+        subject: global.i18n.t("ApplicationDetailComponent_suggestionForBitmarkIos"),
         recipients: ['support@bitmark.com'],
-        body: 'App version: ' + DataProcessor.getApplicationVersion() + ' (' + DataProcessor.getApplicationBuildNumber() + ')',
+        body: global.i18n.t("ApplicationDetailComponent_appVersion") + ' ' + DataProcessor.getApplicationVersion() + ' (' + DataProcessor.getApplicationBuildNumber() + ')',
       }, (error) => {
         if (error) {
           Alert.alert('Error', 'Could not send mail. Please send a mail to support@bitmark.com');
@@ -69,47 +69,47 @@ export class ApplicationDetailComponent extends React.Component {
           <TouchableOpacity style={defaultStyles.headerLeft} onPress={() => this.props.navigation.goBack()} >
             <Image style={defaultStyles.headerLeftIcon} source={require('./../../../../../assets/imgs/header_blue_icon.png')} />
           </TouchableOpacity>
-          <Text style={defaultStyles.headerTitle}>DETAILS</Text>
+          <Text style={defaultStyles.headerTitle}>{global.i18n.t("ApplicationDetailComponent_details")}</Text>
           <TouchableOpacity style={defaultStyles.headerRight} />
         </View>
         <View style={applicationDetailStyle.bodyContent}>
           <View style={applicationDetailStyle.topArea}>
             <TouchableOpacity style={applicationDetailStyle.rowSetting} onPress={() => {
               this.props.navigation.navigate('BitmarkWebView', {
-                title: 'Terms of Service', sourceUrl: config.bitmark_web_site + '/terms?env=app',
+                title: global.i18n.t("ApplicationDetailComponent_termsOfService"), sourceUrl: config.bitmark_web_site + '/terms?env=app',
                 hideBottomController: true,
               })
             }}>
-              <Text style={applicationDetailStyle.itemSettingText}>{'Terms of Service'.toUpperCase()}</Text>
+              <Text style={applicationDetailStyle.itemSettingText}>{global.i18n.t("ApplicationDetailComponent_termsOfService").toUpperCase()}</Text>
             </TouchableOpacity>
             <View style={applicationDetailStyle.lineSetting}></View>
             <TouchableOpacity style={applicationDetailStyle.rowSetting} onPress={() => {
               this.props.navigation.navigate('BitmarkWebView', {
-                title: 'Privacy Policy', sourceUrl: config.bitmark_web_site + '/privacy?env=app',
+                title: global.i18n.t("ApplicationDetailComponent_privacyPolicy"), sourceUrl: config.bitmark_web_site + '/privacy?env=app',
                 hideBottomController: true,
               })
             }}>
-              <Text style={applicationDetailStyle.itemSettingText}>{'Privacy Policy'.toUpperCase()}</Text>
+              <Text style={applicationDetailStyle.itemSettingText}>{global.i18n.t("ApplicationDetailComponent_privacyPolicy").toUpperCase()}</Text>
             </TouchableOpacity>
             <View style={applicationDetailStyle.lineSetting}></View>
           </View>
 
           <View style={applicationDetailStyle.donorInfo}>
-            <Text style={applicationDetailStyle.version}>Version: {DataProcessor.getApplicationVersion()} ({DataProcessor.getApplicationBuildNumber() + (config.network !== 'livenet' ? '-' + config.network : '')})</Text>
+            <Text style={applicationDetailStyle.version}>{global.i18n.t("ApplicationDetailComponent_version")} {DataProcessor.getApplicationVersion()} ({DataProcessor.getApplicationBuildNumber() + (config.network !== 'livenet' ? '-' + config.network : '')})</Text>
           </View>
 
           <View style={applicationDetailStyle.bottomArea}>
             <View style={applicationDetailStyle.lineSetting}></View>
             <TouchableOpacity style={applicationDetailStyle.rowSetting} onPress={() => rateApp()}>
-              <Text style={applicationDetailStyle.itemSettingText}>{'App Store Rating & Review'.toUpperCase()}</Text>
+              <Text style={applicationDetailStyle.itemSettingText}>{global.i18n.t("ApplicationDetailComponent_appStoreRatingAndReview")}</Text>
             </TouchableOpacity>
             <View style={applicationDetailStyle.lineSetting}></View>
             <TouchableOpacity style={applicationDetailStyle.rowSetting} onPress={() => { shareApp() }}>
-              <Text style={applicationDetailStyle.itemSettingText}>{'Share This App'.toUpperCase()}</Text>
+              <Text style={applicationDetailStyle.itemSettingText}>{global.i18n.t("ApplicationDetailComponent_shareThisApp")}</Text>
             </TouchableOpacity>
             <View style={applicationDetailStyle.lineSetting}></View>
             <TouchableOpacity style={[applicationDetailStyle.rowSetting, { marginBottom: constant.blankFooter, }]} onPress={() => { requestSendFeedback() }}>
-              <Text style={applicationDetailStyle.itemSettingText}>{'Send Feedback'.toUpperCase()}</Text>
+              <Text style={applicationDetailStyle.itemSettingText}>{global.i18n.t("ApplicationDetailComponent_sendFeedback")}</Text>
             </TouchableOpacity>
           </View>
         </View>
