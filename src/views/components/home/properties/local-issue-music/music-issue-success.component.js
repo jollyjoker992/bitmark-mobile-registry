45d6d716bdaa05f097ec26483d14aee2b2869f89
bachlx <bachlx@bitmark.com>
2018-12-11 16:26:36 +0700

    update UI

--- src/views/components/home/properties/local-issue-music/music-issue-success.component.js
+++ src/views/components/home/properties/local-issue-music/music-issue-success.component.js
@@ -44,39 +44,43 @@ export class MusicIssueSuccessComponent extends React.Component {
             <View style={cStyles.claimIframe}>
 
             </View>
-            <View>
+            <View style={cStyles.issueResult}>
               {this.state.selected === 'embed' && <View style={cStyles.resultArea}>
-                <View>
-                  <Text>Embed Bitmark</Text>
+                <View style={cStyles.resultHeader}>
+                  <Text style={cStyles.resultHeaderTitle}>Embed Bitmark</Text>
                   <TouchableOpacity onPress={this.copySelectedResult.bind(this)}>
-                    {this.state.copied && <Text>{this.state.copied ? 'COPIED' : 'COPY'}</Text>}
+                    <Text style={cStyles.resultHeaderCopy}>{this.state.copied ? 'COPIED' : 'COPY'}</Text>
                   </TouchableOpacity>
                 </View>
               </View>}
 
               {this.state.selected !== 'embed' && <View style={cStyles.resultArea}>
-                <View>
-                  <Text>Share link</Text>
+                <View style={cStyles.resultHeader}>
+                  <Text style={cStyles.resultHeaderTitle}>Share link</Text>
                   <TouchableOpacity onPress={this.copySelectedResult.bind(this)}>
-                    {this.state.copied && <Text>{this.state.copied ? 'COPIED' : 'COPY'}</Text>}
+                    <Text style={cStyles.resultHeaderCopy}>{this.state.copied ? 'COPIED' : 'COPY'}</Text>
                   </TouchableOpacity>
                 </View>
-                <View style={cStyles.resultContent}>
-                  <Text>test</Text>
-                </View>
               </View>}
+              <View style={cStyles.resultContent}>
+                <Text style={cStyles.resultContentText}>test</Text>
+              </View>
               <View style={cStyles.musicSuccessButtons}>
-                <TouchableOpacity style={[cStyles.musicSuccessButton, { backgroundColor: this.state.selected === 'embed' ? '#E6FF00' : 'white' }]}>
+                <TouchableOpacity style={[cStyles.musicSuccessButton, {
+                  backgroundColor: this.state.selected === 'embed' ? '#E6FF00' : 'white',
+                  borderColor: this.state.selected === 'embed' ? '#E6FF00' : 'white'
+                }]}>
                   <Image style={cStyles.musicSuccessButtonIcon} source={require('assets/imgs/music_embed_icon.png')} />
                 </TouchableOpacity>
-                <TouchableOpacity style={[cStyles.musicSuccessButton, { backgroundColor: this.state.selected === 'embed' ? 'white' : '#E6FF00' }]}>
+                <TouchableOpacity style={[cStyles.musicSuccessButton, {
+                  backgroundColor: this.state.selected === 'embed' ? 'white' : '#E6FF00',
+                  borderColor: this.state.selected === 'embed' ? 'white' : '#E6FF00',
+                  marginLeft: 25,
+                }]}>
                   <Image style={cStyles.musicSuccessButtonIcon} source={require('assets/imgs/music_link_icon.png')} />
                 </TouchableOpacity>
               </View>
             </View>
-          </View>
-          <View>
-            <Text style={cStyles.message}>Distribute your music in only 3 steps!</Text>
           </View>
         </View>
       </View>
@@ -104,35 +108,57 @@ const cStyles = StyleSheet.create({
   mainContent: {
     flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center',
     width: '100%',
+    paddingLeft: convertWidth(19), paddingRight: convertWidth(19),
   },
   title: {
     fontFamily: 'Avenir-Black', fontSize: 24, fontWeight: '900', color: 'white', lineHeight: 33,
     marginLeft: convertWidth(39),
+    width: '100%',
   },
   description: {
     fontFamily: 'Avenir-Light', fontSize: 17, fontWeight: '300', color: 'white', lineHeight: 23,
     marginLeft: convertWidth(39), marginTop: 30,
+    width: '100%',
   },
   claimIframe: {
     width: convertWidth(306), minHeight: 159,
     borderWidth: 1,
+    marginTop: 36,
   },
-  message: {
-    fontFamily: 'Avenir-Medium', fontSize: 15, fontWeight: '300', color: 'white', textAlign: 'center', fontStyle: 'italic',
+  issueResult: {
+    flex: 1,
     width: '100%',
-    marginBottom: 12,
+    marginTop: 50,
   },
   resultArea: {
-
+    width: '100%',
+  },
+  resultHeader: {
+    flexDirection: 'row',
+  },
+  resultHeaderTitle: {
+    fontFamily: 'Avenir-Light', fontSize: 17, fontWeight: '300', color: 'white',
+    flex: 1,
+  },
+  resultHeaderCopy: {
+    fontFamily: 'Avenir-Black', fontSize: 14, fontWeight: '600', color: 'white',
   },
   resultContent: {
-    minHeight: 40,
+    marginTop: 5,
+    minHeight: 40, width: '100%',
     backgroundColor: '#E6FF00',
+    alignItems: 'center', justifyContent: 'center',
+    paddingLeft: 10,
+    borderWidth: 1,
+  },
+  resultContentText: {
+    width: '100%',
   },
 
   musicSuccessButton: {
     width: 60, height: 60,
     borderRadius: 30, borderWidth: 1,
+    marginTop: 19,
   },
   musicSuccessButtons: {
     width: '100%',
