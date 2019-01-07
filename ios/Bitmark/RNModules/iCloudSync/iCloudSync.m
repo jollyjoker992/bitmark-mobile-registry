49dacab46fc085b8781cab6b63356b230bf9a74b
Anh Nguyen <anhnguyen@bitmark.com>
2019-01-07 13:13:00 +0700

    Fix bug: iCloud cannot sync new files

--- ios/Bitmark/RNModules/iCloudSync/iCloudSync.m
+++ ios/Bitmark/RNModules/iCloudSync/iCloudSync.m
@@ -81,7 +81,7 @@ RCT_EXPORT_METHOD(syncCloud:(RCTResponseSenderBlock)callback)
     NSMetadataItem *item = keptFiles[i];
     NSString *downloadStatus = [item valueForAttribute:NSMetadataUbiquitousItemDownloadingStatusKey];
     if (downloadStatus &&
-        [downloadStatus isEqualToString:NSMetadataUbiquitousItemDownloadingStatusDownloaded]) {
+        [downloadStatus isEqualToString:NSMetadataUbiquitousItemDownloadingStatusCurrent]) {
           if (item) {
             NSString *path = [item valueForAttribute:NSMetadataItemPathKey];
             [result setValue:path forKey:keptFileNames[i]];
