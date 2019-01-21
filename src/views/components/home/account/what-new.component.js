9628b90926f1b0054e3c3d37c7eb2446e78c1009
bachlx <bachlx@bitmark.com>
2019-01-21 13:17:45 +0700

    update what new

--- src/views/components/home/account/what-new.component.js
+++ src/views/components/home/account/what-new.component.js
@@ -18,7 +18,7 @@ export class WhatNewComponent extends Component {
   }
   constructor(props) {
     super(props);
-    let releaseDate = moment('03-01-2019', 'DD-MM-YYYY');
+    let releaseDate = moment('21-01-2019', 'DD-MM-YYYY');
     let diffDay = moment().diff(releaseDate, 'days');
     this.state = {
       step: 2,
