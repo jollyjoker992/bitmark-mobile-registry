949f80aba0131f12d5a3158ee2f7a22ef7cd654a
bachlx <bachlx@bitmark.com>
2019-02-22 16:09:29 +0700

    up version + update transfer button

--- src/views/components/home/properties/local-property-transfer/local-property-transfer.component.js
+++ src/views/components/home/properties/local-property-transfer/local-property-transfer.component.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  View, Text, TouchableOpacity, Image, TextInput, ScrollView, SafeAreaView, Keyboard,
+  View, Text, TouchableOpacity, Image, TextInput, ScrollView, SafeAreaView, KeyboardAvoidingView,
+  Keyboard,
+  Platform,
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
@@ -62,48 +64,50 @@ export class LocalPropertyTransferComponent extends React.Component {
   render() {
     return (
       <SafeAreaView style={{ flex: 1, borderBottomWidth: 0.3, backgroundColor: '#F5F5F5' }}>
-        <View style={[defaultStyles.header, { height: constant.headerSize.height }]}>
-          <TouchableOpacity style={defaultStyles.headerLeft} onPress={Actions.pop}>
-            <Image style={defaultStyles.headerLeftIcon} source={require('assets/imgs/header_blue_icon.png')} />
-          </TouchableOpacity>
-          <View style={defaultStyles.headerCenter}>
-            <Text style={[defaultStyles.headerTitle, { maxWidth: convertWidth(180), }]} numberOfLines={1}>{this.state.asset.name} </Text>
-            {this.state.bitmarkIndexNumber && <Text style={[defaultStyles.headerTitle, { maxWidth: convertWidth(180), }]} numberOfLines={1}>({this.state.bitmarkIndexNumber})</Text>}
-          </View>
-          <TouchableOpacity style={[defaultStyles.headerRight]} />
-        </View>
-
-        <View style={propertyTransferStyle.body}>
-          <ScrollView style={propertyTransferStyle.content}>
-            <TouchableOpacity activeOpacity={1} style={propertyTransferStyle.mainContent}>
-              <Text style={propertyTransferStyle.transferTitle}>{global.i18n.t("LocalPropertyTransferComponent_sendBitmark")}</Text>
-              <View style={propertyTransferStyle.inputAccountNumberBar} >
-                <TextInput style={[config.isAndroid ? { padding: 2 } : {}, propertyTransferStyle.inputAccountNumber]} placeholder={global.i18n.t("LocalPropertyTransferComponent_recipientBitmarkAccountNumber")}
-                  onChangeText={(bitmarkAccount) => this.setState({ bitmarkAccount })}
-                  returnKeyType="done"
-                  value={this.state.bitmarkAccount}
-                  onFocus={() => { this.setState({ bitmarkAccountError: false, transferError: '' }) }}
-                />
-                {!!this.state.bitmarkAccount && <TouchableOpacity style={propertyTransferStyle.removeAccountNumberButton} onPress={() => this.setState({ bitmarkAccount: '', bitmarkAccountError: false, transferError: '' })} >
-                  <Image style={propertyTransferStyle.removeAccountNumberIcon} source={require('assets/imgs/remove-icon.png')} />
-                </TouchableOpacity>}
-              </View>
-              <Text style={propertyTransferStyle.accountNumberError}>{this.state.bitmarkAccountError}</Text>
-              <Text style={propertyTransferStyle.transferMessage}>{global.i18n.t("LocalPropertyTransferComponent_transferMessage")}</Text>
-              <Text style={propertyTransferStyle.accountNumberError}>{this.state.transferError}</Text>
+        <KeyboardAvoidingView behavior={Platform.select({ ios: 'padding', android: '' })} style={{ flex: 1 }} >
+          <View style={[defaultStyles.header, { height: constant.headerSize.height }]}>
+            <TouchableOpacity style={defaultStyles.headerLeft} onPress={Actions.pop}>
+              <Image style={defaultStyles.headerLeftIcon} source={require('assets/imgs/header_blue_icon.png')} />
             </TouchableOpacity>
-          </ScrollView>
-        </View>
+            <View style={defaultStyles.headerCenter}>
+              <Text style={[defaultStyles.headerTitle, { maxWidth: convertWidth(180), }]} numberOfLines={1}>{this.state.asset.name} </Text>
+              {this.state.bitmarkIndexNumber && <Text style={[defaultStyles.headerTitle, { maxWidth: convertWidth(180), }]} numberOfLines={1}>({this.state.bitmarkIndexNumber})</Text>}
+            </View>
+            <TouchableOpacity style={[defaultStyles.headerRight]} />
+          </View>
 
-        <TouchableOpacity style={[propertyTransferStyle.sendButton, {
-          borderTopColor: this.state.bitmarkAccount ? '#0060F2' : '#A4B5CD'
-        }]}
-          disabled={!this.state.bitmarkAccount}
-          onPress={this.onSendProperty}>
-          <Text style={[propertyTransferStyle.sendButtonText, {
-            color: this.state.bitmarkAccount ? '#0060F2' : '#C2C2C2'
-          }]}>{global.i18n.t("LocalPropertyTransferComponent_send")}</Text>
-        </TouchableOpacity>
+          <View style={propertyTransferStyle.body}>
+            <ScrollView style={propertyTransferStyle.content}>
+              <TouchableOpacity activeOpacity={1} style={propertyTransferStyle.mainContent}>
+                <Text style={propertyTransferStyle.transferTitle}>{global.i18n.t("LocalPropertyTransferComponent_sendBitmark")}</Text>
+                <View style={propertyTransferStyle.inputAccountNumberBar} >
+                  <TextInput style={[config.isAndroid ? { padding: 2 } : {}, propertyTransferStyle.inputAccountNumber]} placeholder={global.i18n.t("LocalPropertyTransferComponent_recipientBitmarkAccountNumber")}
+                    onChangeText={(bitmarkAccount) => this.setState({ bitmarkAccount })}
+                    returnKeyType="done"
+                    value={this.state.bitmarkAccount}
+                    onFocus={() => { this.setState({ bitmarkAccountError: false, transferError: '' }) }}
+                  />
+                  {!!this.state.bitmarkAccount && <TouchableOpacity style={propertyTransferStyle.removeAccountNumberButton} onPress={() => this.setState({ bitmarkAccount: '', bitmarkAccountError: false, transferError: '' })} >
+                    <Image style={propertyTransferStyle.removeAccountNumberIcon} source={require('assets/imgs/remove-icon.png')} />
+                  </TouchableOpacity>}
+                </View>
+                <Text style={propertyTransferStyle.accountNumberError}>{this.state.bitmarkAccountError}</Text>
+                <Text style={propertyTransferStyle.transferMessage}>{global.i18n.t("LocalPropertyTransferComponent_transferMessage")}</Text>
+                <Text style={propertyTransferStyle.accountNumberError}>{this.state.transferError}</Text>
+              </TouchableOpacity>
+            </ScrollView>
+          </View>
+
+          <TouchableOpacity style={[propertyTransferStyle.sendButton, {
+            borderTopColor: this.state.bitmarkAccount ? '#0060F2' : '#A4B5CD'
+          }]}
+            disabled={!this.state.bitmarkAccount}
+            onPress={this.onSendProperty}>
+            <Text style={[propertyTransferStyle.sendButtonText, {
+              color: this.state.bitmarkAccount ? '#0060F2' : '#C2C2C2'
+            }]}>{global.i18n.t("LocalPropertyTransferComponent_send")}</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
       </SafeAreaView>
     );
   }
