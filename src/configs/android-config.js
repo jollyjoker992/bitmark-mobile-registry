93dc9fe98e2a8b808dd90a5c26d239cacbd9c670
bachlx <bachlx@bitmark.com>
2019-02-26 12:02:50 +0700

    update config for android

--- src/configs/android-config.js
+++ src/configs/android-config.js
@@ -1,6 +1,6 @@
 const androidConfig = {
   isAndroid: true,
-  appLink: '',
+  appLink: 'https://play.google.com/store/apps/details?id=com.bitmark.registry',
   codePush: {
     productionKey: '',
     stagingKey: '',
