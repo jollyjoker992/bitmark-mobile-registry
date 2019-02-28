04480bad1bde9581ec4489ab1080cd9c916f4115
Hieu Pham <hieupham@bitmark.com>
2019-02-28 15:48:15 +0700

    add method for picking video and photo

--- android/app/src/main/java/com/bitmark/registry/modules/navigation/NavigationModule.java
+++ android/app/src/main/java/com/bitmark/registry/modules/navigation/NavigationModule.java
@@ -3,6 +3,7 @@ package com.bitmark.registry.modules.navigation;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 
 import com.bitmark.registry.utils.MediaUtils;
@@ -18,6 +19,8 @@ import static android.app.Activity.RESULT_OK;
 public class NavigationModule extends ReactContextBaseJavaModule {
 
     private static final int READ_DOCUMENT_CODE = 42;
+
+    private static final int READ_MEDIA_CODE = 43;
 
     NavigationModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -53,6 +56,42 @@ public class NavigationModule extends ReactContextBaseJavaModule {
                                          Intent data) {
                 context.removeActivityEventListener(this);
                 if (resultCode == RESULT_OK && requestCode == READ_DOCUMENT_CODE) {
+                    Uri uri = data.getData();
+                    try {
+                        String path = MediaUtils.getAbsolutePathFromUri(context, uri);
+                        if (TextUtils.isEmpty(path)) promise.reject("UNSUPPORT_FILE",
+                                new NativeModuleException("Unsupport file type"));
+                        else
+                            promise.resolve(path);
+                    } catch (Throwable e) {
+                        promise.reject(e);
+                    }
+                }
+            }
+
+            @Override
+            public void onNewIntent(Intent intent) {
+
+            }
+        });
+    }
+
+    @ReactMethod
+    public void browseMedia(String type, Promise promise) throws NativeModuleException {
+        if (!type.equals("photo") || !type.equals("video"))
+            throw new NativeModuleException("Invalid type");
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        Uri uri = type.equals("photo") ? MediaStore.Images.Media.EXTERNAL_CONTENT_URI :
+                type.equals("video") ? MediaStore.Video.Media.EXTERNAL_CONTENT_URI : null;
+        intent.setDataAndType(uri, "*/*");
+        ReactApplicationContext context = getReactApplicationContext();
+        context.startActivityForResult(intent, READ_MEDIA_CODE, null);
+        context.addActivityEventListener(new ActivityEventListener() {
+            @Override
+            public void onActivityResult(Activity activity, int requestCode, int resultCode,
+                                         Intent data) {
+                context.removeActivityEventListener(this);
+                if (resultCode == RESULT_OK && requestCode == READ_MEDIA_CODE) {
                     Uri uri = data.getData();
                     try {
                         String path = MediaUtils.getAbsolutePathFromUri(context, uri);
