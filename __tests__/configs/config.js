25b70449b3df3606411f3f74f448241e34510534
Dung Le <phieudu241@gmail.com>
2019-03-04 16:53:26 +0700

    [Regression][Account] Finish account flow implementation

--- __tests__/configs/config.js
+++ __tests__/configs/config.js
@@ -1,9 +1,16 @@
-const APPIUM_CONFIG = {
+let localConfig = {};
+try {
+    localConfig = require('./local-config');
+} catch (err) {
+    console.log('Local config is not existing');
+}
+
+const APPIUM_CONFIG = localConfig.APPIUM_CONFIG || {
     HOST: 'localhost',
     PORT: 4723
 };
 
-const RUN_CONFIG = {
+const RUN_CONFIG = localConfig.RUN_CONFIG || {
     platformName: 'iOS',
     platformVersion: '11.3',
     deviceName: 'iPhone 6',
@@ -11,8 +18,8 @@ const RUN_CONFIG = {
     app: '/Users/dungle/WebstormProjects/bitmark-mobile-registry/ios/build/Build/Products/Debug-iphonesimulator/Bitmark Registry dev.app' // relative to root of project
 };
 
-const TEST_CONFIG = {
-    DEFAULT_TIMEOUT_INTERVAL: 60000,
+const TEST_CONFIG = localConfig.TEST_CONFIG || {
+    DEFAULT_TIMEOUT_INTERVAL: 100 * 60000,
     APP_LOAD_TIMEOUT: 5000,
     CHANGE_SCREEN_TIMEOUT: 5000
 };
